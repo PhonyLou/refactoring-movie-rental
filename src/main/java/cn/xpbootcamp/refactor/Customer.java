a7c69ad0187e55551507e5a788857fbd9ca3685d
Phony Lou <louqilin555@gmail.com>
2020-05-01 16:37:03 +0800

    Extract determine amount function

--- src/main/java/cn/xpbootcamp/refactor/Customer.java
+++ src/main/java/cn/xpbootcamp/refactor/Customer.java
@@ -29,22 +29,7 @@ public class Customer {
             Rental each = rentals.nextElement();
             //show figures for this rental
             //determine amounts for each line
-            double thisAmount = 0d;
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.HISTORY:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CAMPUS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            double thisAmount = determineAmount(each);
             //add frequent renter points
             frequentRenterPoints++;
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
@@ -61,6 +46,26 @@ public class Customer {
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
         return result.toString();
+    }
+
+    private double determineAmount(Rental each) {
+        double thisAmount = 0d;
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.HISTORY:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CAMPUS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 
 }
